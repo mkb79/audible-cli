50e6f7d53913073de84975b0b4fca20dcac18d8a
mkb79 <mkb79@hackitall.de>
2020-12-20 23:56:34 +0100

    cmd_library: properly close file object
    - use ``with``-statement when open file in write mode

--- src/audible_cli/cmds/cmd_library.py
+++ src/audible_cli/cmds/cmd_library.py
@@ -30,55 +30,51 @@ async def _export_library(auth, **params):
             num_results=1000
         )
 
-    f = pathlib.Path(params.get("output"))
-
-    headers = [
+    headers = (
         "asin", "title", "subtitle", "authors", "narrators", "series_title",
         "series_sequence", "genres", "runtime_length_min", "is_finished",
         "percent_complete", "rating", "num_ratings", "date_added",
         "release_date", "cover_url"
-    ]
-
-    writer = csv.DictWriter(
-        f.open("w", encoding="utf-8"), fieldnames=headers, dialect="excel-tab"
     )
-    writer.writeheader()
-
     keys_with_raw_values = (
         "asin", "title", "subtitle", "runtime_length_min", "is_finished",
         "percent_complete", "release_date"
     )
 
-    for item in library:
-        data_row = {}
-        for key in item:
-            v = getattr(item, key)
-            if v is None:
-                pass
-            elif key in keys_with_raw_values:
-                data_row[key] = v
-            elif key in ("authors", "narrators"):
-                data_row[key] = ", ".join([i["name"] for i in v])
-            elif key == "series":
-                data_row["series_title"] = v[0]["title"]
-                data_row["series_sequence"] = v[0]["sequence"]
-            elif key == "rating":
-                data_row["rating"] = v["overall_distribution"][
-                    "display_average_rating"]
-                data_row["num_ratings"] = v["overall_distribution"][
-                    "num_ratings"]
-            elif key == "library_status":
-                data_row["date_added"] = v["date_added"]
-            elif key == "product_images":
-                data_row["cover_url"] = v["500"]
-            elif key == "category_ladders":
-                genres = []
-                for genre in v:
-                    for ladder in genre["ladder"]:
-                        genres.append(ladder["name"])
-                data_row["genres"] = ", ".join(genres)
+    with pathlib.Path(params.get("output")).open("w", encoding="utf-8") as f:
+        writer = csv.DictWriter(f, fieldnames=headers, dialect="excel-tab")
+        writer.writeheader()
 
-        writer.writerow(data_row)
+        for item in library:
+            data_row = {}
+            for key in item:
+                v = getattr(item, key)
+                if v is None:
+                    pass
+                elif key in keys_with_raw_values:
+                    data_row[key] = v
+                elif key in ("authors", "narrators"):
+                    data_row[key] = ", ".join([i["name"] for i in v])
+                elif key == "series":
+                    data_row["series_title"] = v[0]["title"]
+                    data_row["series_sequence"] = v[0]["sequence"]
+                elif key == "rating":
+                    data_row["rating"] = v["overall_distribution"][
+                        "display_average_rating"]
+                    data_row["num_ratings"] = v["overall_distribution"][
+                        "num_ratings"]
+                elif key == "library_status":
+                    data_row["date_added"] = v["date_added"]
+                elif key == "product_images":
+                    data_row["cover_url"] = v["500"]
+                elif key == "category_ladders":
+                    genres = []
+                    for genre in v:
+                        for ladder in genre["ladder"]:
+                            genres.append(ladder["name"])
+                    data_row["genres"] = ", ".join(genres)
+    
+            writer.writerow(data_row)
 
 
 @cli.command("export")
