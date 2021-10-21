16773b7982578ac60d5039edfb7c9b15198bb756
mkb79 <mkb79@hackitall.de>
2021-10-21 23:29:02 +0200

    Bump version to v0.0.2

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ setup(
     ],
     install_requires=[
         "aiofiles",
-        "audible>=0.5.5",
+        "audible==0.6.0",
         "click",
         "colorama; platform_system=='Windows'",
         "httpx>=0.20.*",
