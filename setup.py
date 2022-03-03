d60c0bcf16d5496e60181c25cce4ede55b7c8811
mkb79 <mkb79@hackitall.de>
2022-03-03 20:09:28 +0100

    Fix a log issue

--- setup.py
+++ setup.py
@@ -48,6 +48,7 @@ setup(
         "aiofiles",
         "audible==0.7.*",
         "click>=7",
+        "click-log",
         "colorama; platform_system=='Windows'",
         "httpx>=0.20.*",
         "Pillow",
