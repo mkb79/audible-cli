0c0506a14eb4875fce97755395d6ebe8e591dc71
mkb79 <mkb79@hackitall.de>
2022-03-27 19:40:06 +0200

    bump Audible to v0.7.1

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ setup(
     ],
     install_requires=[
         "aiofiles",
-        "audible==0.7.*",
+        "audible==0.7.1",
         "click>=8",
         "colorama; platform_system=='Windows'",
         "httpx==0.20.*",
