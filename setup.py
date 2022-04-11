b06426ae57e8323450dc6a58975c0d6001c01e55
mkb79 <mkb79@hackitall.de>
2022-04-11 15:45:20 +0200

    Bugfix httpx version requirement

--- setup.py
+++ setup.py
@@ -49,7 +49,7 @@ setup(
         "audible==0.8.0",
         "click>=8",
         "colorama; platform_system=='Windows'",
-        "httpx>=0.20.*,<=0.22.*",
+        "httpx>=0.20.0,<0.23.0",
         "packaging",
         "Pillow",
         "tabulate",
