0731e54184fd8357f430d1a757c27abbdf671689
mkb79 <mkb79@hackitall.de>
2022-04-25 12:56:39 +0200

    rework cmd_library.py

--- src/audible_cli/cmds/cmd_library.py
+++ src/audible_cli/cmds/cmd_library.py
@@ -1,3 +1,4 @@
+import asyncio
 import csv
 import json
 import pathlib
@@ -9,7 +10,8 @@ from ..decorators import (
     bunch_size_option,
     run_async,
     timeout_option,
-    pass_session
+    pass_session,
+    wrap_async
 )
 from ..models import Library
 from ..utils import export_to_csv
@@ -40,15 +42,30 @@ async def _get_library(session):
     return library
 
 
-def _prepare_library_for_export(library: Library):
-    keys_with_raw_values = (
-        "asin", "title", "subtitle", "runtime_length_min", "is_finished",
-        "percent_complete", "release_date"
-    )
+@cli.command("export")
+@click.option(
+    "--output", "-o",
+    type=click.Path(path_type=pathlib.Path),
+    default=pathlib.Path().cwd() / r"library.{format}",
+    show_default=True,
+    help="output file"
+)
+@timeout_option
+@click.option(
+    "--format", "-f",
+    type=click.Choice(["tsv", "csv", "json"]),
+    default="tsv",
+    show_default=True,
+    help="Output format"
+)
+@bunch_size_option
+@pass_session
+@run_async
+async def export_library(session, **params):
+    """export library"""
 
-    prepared_library = []
-
-    for item in library:
+    @wrap_async
+    def _prepare_item(item):
         data_row = {}
         for key in item:
             v = getattr(item, key)
@@ -78,34 +95,8 @@ def _prepare_library_for_export(library: Library):
                         genres.append(ladder["name"])
                 data_row["genres"] = ", ".join(genres)
 
-        prepared_library.append(data_row)
+        return data_row
 
-    prepared_library.sort(key=lambda x: x["asin"])
-
-    return prepared_library
-
-
-@cli.command("export")
-@click.option(
-    "--output", "-o",
-    type=click.Path(path_type=pathlib.Path),
-    default=pathlib.Path().cwd() / r"library.{format}",
-    show_default=True,
-    help="output file"
-)
-@timeout_option
-@click.option(
-    "--format", "-f",
-    type=click.Choice(["tsv", "csv", "json"]),
-    default="tsv",
-    show_default=True,
-    help="Output format"
-)
-@bunch_size_option
-@pass_session
-@run_async
-async def export_library(session, **params):
-    """export library"""
     output_format = params.get("format")
     output_filename: pathlib.Path = params.get("output")
     if output_filename.suffix == r".{format}":
@@ -114,23 +105,32 @@ async def export_library(session, **params):
 
     library = await _get_library(session)
 
-    prepared_library = _prepare_library_for_export(library)
-
-    headers = (
-        "asin", "title", "subtitle", "authors", "narrators", "series_title",
-        "series_sequence", "genres", "runtime_length_min", "is_finished",
-        "percent_complete", "rating", "num_ratings", "date_added",
-        "release_date", "cover_url"
+    keys_with_raw_values = (
+        "asin", "title", "subtitle", "runtime_length_min", "is_finished",
+        "percent_complete", "release_date"
     )
+
+    prepared_library = await asyncio.gather(
+        *[_prepare_item(i) for i in library]
+    )
+    prepared_library.sort(key=lambda x: x["asin"])
 
     if output_format in ("tsv", "csv"):
         if output_format == csv:
             dialect = "excel"
         else:
             dialect = "excel-tab"
+
+        headers = (
+            "asin", "title", "subtitle", "authors", "narrators", "series_title",
+            "series_sequence", "genres", "runtime_length_min", "is_finished",
+            "percent_complete", "rating", "num_ratings", "date_added",
+            "release_date", "cover_url"
+        )
+
         export_to_csv(output_filename, prepared_library, headers, dialect)
 
-    if output_format == "json":
+    elif output_format == "json":
         data = json.dumps(prepared_library, indent=4)
         output_filename.write_text(data)
 
@@ -142,26 +142,30 @@ async def export_library(session, **params):
 @run_async
 async def list_library(session):
     """list titles in library"""
-    library = await _get_library(session)
 
-    books = []
+    @wrap_async
+    def _prepare_item(item):
+        fields = [item.asin]
 
-    for item in library:
-        asin = item.asin
         authors = ", ".join(
             sorted(a["name"] for a in item.authors) if item.authors else ""
         )
+        if authors:
+            fields.append(authors)
+
         series = ", ".join(
             sorted(s["title"] for s in item.series) if item.series else ""
         )
-        title = item.title
-        books.append((asin, authors, series, title))
-
-    for asin, authors, series, title in sorted(books):
-        fields = [asin]
-        if authors:
-            fields.append(authors)
         if series:
             fields.append(series)
-        fields.append(title)
-        echo(": ".join(fields))
+
+        fields.append(item.title)
+        return ": ".join(fields)
+
+    library = await _get_library(session)
+    books = await asyncio.gather(
+        *[_prepare_item(i) for i in library]
+    )
+
+    for i in sorted(books):
+        echo(i)
