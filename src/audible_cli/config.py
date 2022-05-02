b26ef99332d3518483d77403a15ba53e2904a448
mkb79 <mkb79@hackitall.de>
2022-05-02 20:02:16 +0200

    config.py: fix PEP 8: E125

--- src/audible_cli/config.py
+++ src/audible_cli/config.py
@@ -335,7 +335,7 @@ class Session:
             profile: str,
             password: Optional[str] = None,
             **kwargs
-        ) -> AsyncClient:
+    ) -> AsyncClient:
         auth = self.get_auth_for_profile(profile, password)
         kwargs.setdefault("timeout", self.params.get("timeout", 5))
         return AsyncClient(auth=auth, **kwargs)        
