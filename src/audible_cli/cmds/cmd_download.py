194545e2d01b9ee50aa3955212b309ffaf18f441
mkb79 <mkb79@hackitall.de>
2022-05-02 22:11:17 +0200

    Update cmd_download.py

--- src/audible_cli/cmds/cmd_download.py
+++ src/audible_cli/cmds/cmd_download.py
@@ -3,9 +3,7 @@ import asyncio.log
 import asyncio.sslproto
 import json
 import pathlib
-import ssl
 import logging
-import sys
 
 import aiofiles
 import click
@@ -27,58 +25,9 @@ from ..utils import Downloader
 
 logger = logging.getLogger("audible_cli.cmds.cmd_download")
 
-SSL_PROTOCOLS = (asyncio.sslproto.SSLProtocol,)
-
 CLIENT_HEADERS = {
     "User-Agent": "Audible/671 CFNetwork/1240.0.4 Darwin/20.6.0"
 }
-
-
-def ignore_httpx_ssl_eror(loop):
-    """Ignore aiohttp #3535 / cpython #13548 issue with SSL data after close
-
-    There is an issue in Python 3.7 up to 3.7.3 that over-reports a
-    ssl.SSLError fatal error (ssl.SSLError: [SSL: KRB5_S_INIT] application data
-    after close notify (_ssl.c:2609)) after we are already done with the
-    connection. See GitHub issues aio-libs/aiohttp#3535 and
-    python/cpython#13548.
-
-    Given a loop, this sets up an exception handler that ignores this specific
-    exception, but passes everything else on to the previous exception handler
-    this one replaces.
-
-    Checks for fixed Python versions, disabling itself when running on 3.7.4+
-    or 3.8.
-
-    """
-    if sys.version_info >= (3, 7, 4):
-        return
-
-    orig_handler = loop.get_exception_handler()
-
-    def ignore_ssl_error(context):
-        if context.get("message") in {
-            "SSL error in data received",
-            "Fatal error on transport",
-        }:
-            # validate we have the right exception, transport and protocol
-            exception = context.get("exception")
-            protocol = context.get("protocol")
-            if (
-                    isinstance(exception, ssl.SSLError)
-                    and exception.reason == "KRB5_S_INIT"
-                    and isinstance(protocol, SSL_PROTOCOLS)
-            ):
-                if loop.get_debug():
-                    asyncio.log.logger.debug(
-                        "Ignoring httpx SSL KRB5_S_INIT error")
-                return
-        if orig_handler is not None:
-            orig_handler(loop, context)
-        else:
-            loop.default_exception_handler(context)
-
-    loop.set_exception_handler(ignore_ssl_error)
 
 
 class DownloadCounter:
